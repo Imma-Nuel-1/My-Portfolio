77f6d159c0b29e389cae57a428d4e00c7ec332f1
Lexicon <iyiolad21@gmail.com>
2024-08-15 16:18:43 +0100

    image link was deleted

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
             height={350}
             width={350}
             className="text-[26px] flex  items-center justify-center font-semibold text-white rounded-3xl p-6 border-2 border-slate-800 b_glow"
-            src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1723246442/my_pictures_efytdd.jpg"
+            src=""
             alt=""
           />
           <img
