d5c10c6643bad22ce2c84b54b30004a4da2edd47
Lexicon <iyiolad21@gmail.com>
2024-08-20 23:07:16 +0100

    image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
             height={350}
             width={350}
             className="text-[26px] flex  items-center justify-center font-semibold text-white rounded-3xl p-6 border-2 border-slate-800 b_glow"
-            src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1724191141/Screenshot_20-8-2024_224757_vercel.com_saqjug.jpg"
+            src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1724191598/Screenshot_20-8-2024_23622_vercel.com_jlohoe.jpg"
             alt=""
           />
           <img
